1e83b834424c93e44b1b39bc0fd521286fc19fe3
asgaralipq <asgaralipq@gmail.com>
2022-02-28 04:59:16 +0530

    Added single row case

--- DP/MaxPathGoldL2R.java
+++ DP/MaxPathGoldL2R.java
@@ -16,10 +16,12 @@ public class MaxPathGoldL2R{
 
         int dp[][] = new int[n][m];
 
-        for(int j = n - 1; j >= 0; j--){
-            for(int i = m - 1; i >= 0; i--){
+        for(int j = m - 1; j >= 0; j--){
+            for(int i = n - 1; i >= 0; i--){
                 if( j == m - 1 ){
                     dp[i][j] = arr[i][j];
+                } else if( i == 0 && n == 1){
+                    dp[i][j] = dp[i][j+1] + arr[i][j]; 
                 } else if( i == 0 ){
                     dp[i][j] = Math.max(dp[i][j+1], dp[i+1][j+1]) + arr[i][j];
                 } else if( i == n - 1 ){
